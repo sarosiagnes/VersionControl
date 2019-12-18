0ee008894a7774381ccb2e4caf03dddc772cb10f
Ágnes Buza-Sárosi <agnes.sarosi2@stud.uni-corvinus.hu>
2019-12-18 15:54:46 +0100

    Fájlba mentés

--- UM/UM/Form1.cs
+++ UM/UM/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UM
             InitializeComponent();
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.SaveFile;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -35,6 +37,23 @@ namespace UM
             };
             users.Add(u);
         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "txt";
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(";");
+                    sw.Write(u.FullName);
+                    sw.WriteLine();
+                }
+            
+
         }
     }
 }
