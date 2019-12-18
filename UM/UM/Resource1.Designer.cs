23ed05077dc955c95d62085e346179715bbb5c77
Ágnes Buza-Sárosi <agnes.sarosi2@stud.uni-corvinus.hu>
2019-12-18 15:35:52 +0100

    kalózkodás

--- UM/UM/Resource1.Designer.cs
+++ UM/UM/Resource1.Designer.cs
@@ -70,7 +70,7 @@ namespace UM {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Keresztnév.
+        ///   Looks up a localized string similar to Utónév.
         /// </summary>
         internal static string FirstName {
             get {
